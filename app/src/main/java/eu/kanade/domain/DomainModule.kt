3e2e8e4dcba8d1fe7a3037abf9ef7d9238aa7ee8
NarwhalHorns <onefailedgamer@gamil.com>
2025-04-16 10:54:21 +0100

    backup and restore hidden duplicates

--- app/src/main/java/eu/kanade/domain/DomainModule.kt
+++ app/src/main/java/eu/kanade/domain/DomainModule.kt
@@ -38,6 +38,7 @@ import mihon.domain.extensionrepo.service.ExtensionRepoService
 import mihon.domain.upcoming.interactor.GetUpcomingManga
 import tachiyomi.data.category.CategoryRepositoryImpl
 import tachiyomi.data.chapter.ChapterRepositoryImpl
+import tachiyomi.data.hiddenDuplicates.HiddenDuplicateRepositoryImpl
 import tachiyomi.data.history.HistoryRepositoryImpl
 import tachiyomi.data.manga.MangaRepositoryImpl
 import tachiyomi.data.release.ReleaseServiceImpl
@@ -63,6 +64,7 @@ import tachiyomi.domain.chapter.interactor.SetMangaDefaultChapterFlags
 import tachiyomi.domain.chapter.interactor.ShouldUpdateDbChapter
 import tachiyomi.domain.chapter.interactor.UpdateChapter
 import tachiyomi.domain.chapter.repository.ChapterRepository
+import tachiyomi.domain.hiddenDuplicates.repository.HiddenDuplicateRepository
 import tachiyomi.domain.history.interactor.GetHistory
 import tachiyomi.domain.history.interactor.GetNextChapters
 import tachiyomi.domain.history.interactor.GetTotalReadDuration
@@ -120,7 +122,6 @@ class DomainModule : InjektModule {
         addSingletonFactory<MangaRepository> { MangaRepositoryImpl(get()) }
         addFactory { GetDuplicateLibraryManga(get()) }
         addFactory { AddHiddenDuplicate(get()) }
-        addFactory { GetHiddenDuplicates(get()) }
         addFactory { RemoveHiddenDuplicates(get()) }
         addFactory { GetFavorites(get()) }
         addFactory { GetLibraryManga(get()) }
@@ -140,6 +141,9 @@ class DomainModule : InjektModule {
         addFactory { SetMangaCategories(get()) }
         addFactory { GetExcludedScanlators(get()) }
         addFactory { SetExcludedScanlators(get()) }
+
+        addSingletonFactory<HiddenDuplicateRepository> { HiddenDuplicateRepositoryImpl(get()) }
+        addFactory { GetHiddenDuplicates(get(), get()) }
 
         addSingletonFactory<ReleaseService> { ReleaseServiceImpl(get(), get()) }
         addFactory { GetApplicationRelease(get(), get()) }
