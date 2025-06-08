8c4cf32505973b0b126fca781f737c960d6cfc92
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-06-08 14:22:41 +0800

    Update SourceRepositoryImpl.kt

--- data/src/main/java/tachiyomi/data/source/SourceRepositoryImpl.kt
+++ data/src/main/java/tachiyomi/data/source/SourceRepositoryImpl.kt
@@ -1,97 +1,97 @@
 package tachiyomi.data.source
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import eu.kanade.tachiyomi.source.CatalogueSource
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.online.HttpSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import tachiyomi.data.DatabaseHandler
+import tachiyomi.domain.manga.interactor.NetworkToLocalManga
+import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.source.model.SourceWithCount
-import tachiyomi.domain.source.model.StubSource
-import tachiyomi.domain.source.repository.SourcePagingSource
 import tachiyomi.domain.source.repository.SourceRepository
 import tachiyomi.domain.source.service.SourceManager
-import tachiyomi.domain.source.model.Source as DomainSource
 
 class SourceRepositoryImpl(
     private val sourceManager: SourceManager,
+    private val networkToLocalManga: NetworkToLocalManga,
     private val handler: DatabaseHandler,
 ) : SourceRepository {
 
-    override fun getSources(): Flow<List<DomainSource>> {
-        return sourceManager.catalogueSources.map { sources ->
-            sources.map {
-                mapSourceToDomainSource(it).copy(
-                    supportsLatest = it.supportsLatest,
-                )
-            }
-        }
+    override fun getSources(): Flow<List<Source>> {
+        return sourceManager.getSources()
     }
 
-    override fun getOnlineSources(): Flow<List<DomainSource>> {
-        return sourceManager.catalogueSources.map { sources ->
-            sources
-                .filterIsInstance<HttpSource>()
-                .map(::mapSourceToDomainSource)
-        }
+    override fun getOnlineSources(): Flow<List<Source>> {
+        return sourceManager.getOnlineSources()
     }
 
-    override fun getSourcesWithFavoriteCount(): Flow<List<Pair<DomainSource, Long>>> {
-        return combine(
-            handler.subscribeToList { mangasQueries.getSourceIdWithFavoriteCount() },
-            sourceManager.catalogueSources,
-        ) { sourceIdWithFavoriteCount, _ -> sourceIdWithFavoriteCount }
-            .map {
-                it.map { (sourceId, count) ->
+    override fun getSourcesWithFavoriteCount(): Flow<List<SourceWithCount>> {
+        return handler.subscribeToList { mangasQueries.getSourceIdWithFavoriteCount() }
+            .map { list ->
+                list.map { (sourceId, count) ->
                     val source = sourceManager.getOrStub(sourceId)
-                    val domainSource = mapSourceToDomainSource(source).copy(
-                        isStub = source is StubSource,
-                    )
-                    domainSource to count
+                    SourceWithCount(source, count)
                 }
             }
     }
 
     override fun getSourcesWithNonLibraryManga(): Flow<List<SourceWithCount>> {
-        val sourceIdWithNonLibraryManga =
-            handler.subscribeToList { mangasQueries.getSourceIdsWithNonLibraryManga() }
-        return sourceIdWithNonLibraryManga.map { sourceId ->
-            sourceId.map { (sourceId, count) ->
-                val source = sourceManager.getOrStub(sourceId)
-                val domainSource = mapSourceToDomainSource(source).copy(
-                    isStub = source is StubSource,
-                )
-                SourceWithCount(domainSource, count)
+        return handler.subscribeToList { mangasQueries.getSourceIdsWithNonLibraryManga() }
+            .map { list ->
+                list.map { (sourceId, count) ->
+                    val source = sourceManager.getOrStub(sourceId)
+                    SourceWithCount(source, count)
+                }
             }
-        }
     }
 
     override fun search(
-        sourceId: Long,
+        source: CatalogueSource,
         query: String,
         filterList: FilterList,
-    ): SourcePagingSource {
-        val source = sourceManager.get(sourceId) as CatalogueSource
-        return SourceSearchPagingSource(source, query, filterList)
+    ): Flow<PagingData<Manga>> {
+        return Pager(
+            config = PagingConfig(pageSize = 25),
+            pagingSourceFactory = {
+                object : SourcePagingSource() {
+                    override val source: CatalogueSource = source
+                    override val query: String = query
+                    override val filterList: FilterList = filterList
+                    override val networkToLocalManga: NetworkToLocalManga = this@SourceRepositoryImpl.networkToLocalManga
+                }
+            },
+        ).flow
     }
 
-    override fun getPopular(sourceId: Long): SourcePagingSource {
-        val source = sourceManager.get(sourceId) as CatalogueSource
-        return SourcePopularPagingSource(source)
+    override fun getPopular(source: CatalogueSource): Flow<PagingData<Manga>> {
+        return Pager(
+            config = PagingConfig(pageSize = 25),
+            pagingSourceFactory = {
+                object : SourcePagingSource() {
+                    override val source: CatalogueSource = source
+                    override val query: String = ""
+                    override val filterList: FilterList = FilterList()
+                    override val networkToLocalManga: NetworkToLocalManga = this@SourceRepositoryImpl.networkToLocalManga
+                }
+            },
+        ).flow
     }
 
-    override fun getLatest(sourceId: Long): SourcePagingSource {
-        val source = sourceManager.get(sourceId) as CatalogueSource
-        return SourceLatestPagingSource(source)
+    override fun getLatest(source: CatalogueSource): Flow<PagingData<Manga>> {
+        return Pager(
+            config = PagingConfig(pageSize = 25),
+            pagingSourceFactory = {
+                object : SourcePagingSource() {
+                    override val source: CatalogueSource = source
+                    override val query: String = ""
+                    override val filterList: FilterList = FilterList()
+                    override val networkToLocalManga: NetworkToLocalManga = this@SourceRepositoryImpl.networkToLocalManga
+                }
+            },
+        ).flow
     }
-
-    private fun mapSourceToDomainSource(source: Source): DomainSource = DomainSource(
-        id = source.id,
-        lang = source.lang,
-        name = source.name,
-        supportsLatest = false,
-        isStub = false,
-    )
 }
