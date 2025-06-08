88a122cf01fea86880b37cc6d37de5d6719e8260
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-06-08 14:30:22 +0800

    Update GetDuplicateLibraryManga.kt

--- domain/src/main/java/tachiyomi/domain/manga/interactor/GetDuplicateLibraryManga.kt
+++ domain/src/main/java/tachiyomi/domain/manga/interactor/GetDuplicateLibraryManga.kt
@@ -1,14 +1,17 @@
 package tachiyomi.domain.manga.interactor
 
 import tachiyomi.domain.manga.model.Manga
-import tachiyomi.domain.manga.model.MangaWithChapterCount
 import tachiyomi.domain.manga.repository.MangaRepository
 
 class GetDuplicateLibraryManga(
     private val mangaRepository: MangaRepository,
 ) {
 
-    suspend operator fun invoke(manga: Manga): List<MangaWithChapterCount> {
-        return mangaRepository.getDuplicateLibraryManga(manga.id, manga.title.lowercase())
+    suspend fun await(manga: Manga): List<Manga> {
+        return mangaRepository.getDuplicateLibraryManga(manga.id, manga.title)
+    }
+
+    suspend fun await(mangaId: Long, mangaTitle: String): List<Manga> {
+        return mangaRepository.getDuplicateLibraryManga(mangaId, mangaTitle)
     }
 }
