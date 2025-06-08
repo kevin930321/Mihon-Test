63743f5d10d9be60f371c7305cf95973143662c2
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-06-08 13:49:25 +0800

    Update MangaRepository.kt

--- domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
+++ domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
@@ -1,6 +1,5 @@
 package tachiyomi.domain.manga.repository
 
-// [修正] 新增了所有必要的 import 語句
 import kotlinx.coroutines.flow.Flow
 import tachiyomi.domain.library.model.LibraryManga
 import tachiyomi.domain.manga.model.Manga
@@ -19,8 +18,6 @@ interface MangaRepository {
 
     suspend fun getFavorites(): List<Manga>
 
-    suspend fun getReadMangaNotInLibrary(): List<Manga>
-
     suspend fun getLibraryManga(): List<LibraryManga>
 
     fun getLibraryMangaAsFlow(): Flow<List<LibraryManga>>
@@ -29,18 +26,17 @@ interface MangaRepository {
 
     suspend fun getDuplicateLibraryManga(id: Long, title: String): List<MangaWithChapterCount>
 
+    suspend fun getReadMangaNotInLibrary(): List<Manga>
+
     suspend fun getUpcomingManga(statuses: Set<Long>): Flow<List<Manga>>
 
     suspend fun resetViewerFlags(): Boolean
 
     suspend fun setMangaCategories(mangaId: Long, categoryIds: List<Long>)
 
+    suspend fun insert(manga: Manga): Long?
+
     suspend fun update(update: MangaUpdate): Boolean
 
     suspend fun updateAll(mangaUpdates: List<MangaUpdate>): Boolean
-
-    // [修正] 確保介面與實作同步，補上這兩個函數定義
-    suspend fun insert(manga: Manga): Long?
-
-    suspend fun insertNetworkManga(manga: List<Manga>): List<Manga>
 }
