64f4459d371af0803032458f8c0997bbe00341ca
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-06-08 13:58:52 +0800

    Create SetCustomMangaInfo.kt

---
+++ app/src/main/java/eu/kanade/domain/manga/interactor/SetCustomMangaInfo.kt
@@ -0,0 +1,24 @@
+package eu.kanade.domain.manga.interactor
+
+import tachiyomi.domain.manga.model.MangaUpdate
+
+class SetCustomMangaInfo(
+    private val updateManga: UpdateManga,
+) {
+    suspend fun await(
+        mangaId: Long,
+        customTitle: String?,
+        customAuthor: String?,
+        customArtist: String?,
+        customDescription: String?,
+    ): Boolean {
+        val update = MangaUpdate(
+            id = mangaId,
+            customTitle = customTitle,
+            customAuthor = customAuthor,
+            customArtist = customArtist,
+            customDescription = description,
+        )
+        return updateManga.await(update)
+    }
+}
