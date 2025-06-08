4b1957b60e5851ad401d26f2af96dedb41d3cb72
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-06-08 11:45:35 +0800

    Update SetCustomMangaInfo.kt

--- domain/src/main/java/tachiyomi/domain/manga/interactor/SetCustomMangaInfo.kt
+++ domain/src/main/java/tachiyomi/domain/manga/interactor/SetCustomMangaInfo.kt
@@ -1,3 +1,9 @@
+package tachiyomi.domain.manga.interactor
+
+// [修正] 新增了這兩個關鍵的 import 語句。
+// 這會告訴編譯器去哪裡尋找 MangaUpdate 和 UpdateManga 的定義，解決 "Unresolved reference" 錯誤。
+import tachiyomi.domain.manga.model.MangaUpdate
+
 class SetCustomMangaInfo(
     private val updateManga: UpdateManga,
 ) {
@@ -15,6 +21,7 @@ class SetCustomMangaInfo(
             customArtist = customArtist,
             customDescription = customDescription,
         )
+        // 現在因為 import 了 UpdateManga，這裡的 await 就可以被正確解析了
         return updateManga.await(update)
     }
 }
